6b47d0e75047012d878c380b13e2e2a3e4c4d2c4
Van Tien <vantiennst@gmail.com>
2024-03-12 15:01:28 +0700

    update player, tilemap

--- game.py
+++ game.py
@@ -2,21 +2,35 @@ import pygame
 import sys
 
 from scripts.supports import load_image, load_images
+from scripts.entities import PhysicsEntity
+from scripts.tilemap import TileMap
+
 
 class Game():
     def __init__(self, screen_width, screen_height, fps):
         pygame.init()
         pygame.display.set_caption("Ninja Game")
         self.screen = pygame.display.set_mode((screen_width, screen_height))
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
         self.fps = fps
 
-        self.assest = {
+        self.assets = {
             'background': load_image('background.png'),
+            'player': load_image('entities\\player.png'),
+            'decor': load_images('tiles\\decor'),
+            'grass': load_images('tiles\\grass'),
+            'large_decor': load_images('tiles\\large_decor'),
+            'spawners': load_images('tiles\\spawners'),
+            'stone': load_images('tiles\\stone'),
         }
-        self.img_pos = [160, 260]
+
+        self.player = PhysicsEntity(self, (60, 60), (8, 15))
+        self.tilemap = TileMap(self, tile_size=16)
+
         self.movement = [False, False]
-        
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -24,22 +38,31 @@ class Game():
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_SPACE:
+                        self.player.velocity[1] = -4
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-            
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-                    
-            self.screen.blit(pygame.transform.scale(self.assest['background'], (self.screen.get_width(), self.screen.get_height())), (0,0))  
+
+            self.player.update(self.tilemap,
+                               (self.movement[1] - self.movement[0], 0))
+
+            self.display.blit(self.assets['background'], (0, 0))
+            self.tilemap.render(self.display)
+            self.player.render(self.display)
+
+            self.screen.blit(pygame.transform.scale(
+                self.display, (self.screen.get_width(), self.screen.get_height())), (0, 0))
 
             pygame.display.update()
             self.clock.tick(self.fps)
 
-game = Game(960,720,60)
+
+game = Game(960, 720, 60)
 game.run()
--- scripts/supports.py
+++ scripts/supports.py
@@ -5,14 +5,13 @@ DEFAULT_IMG_PATH = 'data\\images\\'
 
 
 def load_image(path):
-    image = pygame.image.load(DEFAULT_IMG_PATH + path)
-    image.set_colorkey((0,0,0))
+    image = pygame.image.load(DEFAULT_IMG_PATH + path).convert()
+    image.set_colorkey((0, 0, 0))
     return image
-    
-    
+
+
 def load_images(path):
     images = []
-    for image_name in os.listdir(DEFAULT_IMG_PATH + path):
-        images.append(load_image(path + "\\" + image_name))    
+    for image_name in sorted(os.listdir(DEFAULT_IMG_PATH + path)):
+        images.append(load_image(path + "\\" + image_name))
     return images
-
